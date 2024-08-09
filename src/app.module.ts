8b0edec5e5b6b4a2fdc9ed6150c9a2dabd3b3375
julia <julialameiras@gmail.com>
2024-08-09 09:59:26 -0300

    chore:config the main class and app.module

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_projeto_bloco_02',
+      entities: [],
+      synchronize: true
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
